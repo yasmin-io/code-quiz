979548057cc3d687cf0f717888ace4f6b85741b4
Yasmin <yasmin.bunnie@gmail.com>
2022-01-12 02:18:56 +0000

    Added: Start Game function to button in javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,12 @@
+var startButton = document.querySelector(".start-button");
 
+function startGame(event) {
+  event.preventDefault();
+  console.log("Game Started");
+}
+
+function nextQuestion() {}
+
+function selectAnswer() {}
+
+startButton.addEventListener("click", startGame);
