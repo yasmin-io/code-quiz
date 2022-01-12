3c799023095590a7cf8df2f0a8f5d1142b437c90
Yasmin <yasmin.bunnie@gmail.com>
2022-01-12 19:50:53 +0000

    add logic for asking and answering questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,15 +21,58 @@ var quizQuestions = [
   },
 ];
 
-var startButton = document.querySelector(".start-button");
+var questionIndex = 0;
+
+var startPage = document.getElementById("start-page");
+var questionPage = document.getElementById("questions-page");
+var endPage = document.getElementById("end-page");
+
+var startButton = document.getElementById("start-quiz-button");
+var questionTitle = document.getElementById("question-title");
+var questionAnswersSection = document.getElementById("question-answers");
 
 function startGame(event) {
   event.preventDefault();
   console.log("Game Started");
+
+  startPage.setAttribute("class", "hide");
+  questionPage.removeAttribute("class");
+
+  //set timer
+
+  nextQuestion();
 }
 
-function nextQuestion() {}
+function nextQuestion() {
+  var questionObject = quizQuestions[questionIndex];
 
-function selectAnswer() {}
+  console.log(questionObject);
+
+  questionTitle.textContent = questionObject.question;
+
+  questionObject.answers.forEach(function (answer) {
+    var answerButton = document.createElement("button");
+    answerButton.setAttribute("value", answer);
+
+    answerButton.textContent = answer;
+    console.log(answerButton);
+    answerButton.onclick = selectAnswer;
+    questionAnswersSection.appendChild(answerButton);
+  });
+}
+
+function selectAnswer() {
+  console.log("Button clicked: ", this.value);
+  if (this.value !== quizQuestions[questionIndex].correctAnswer) {
+    console.log("User selected the incorrect answer");
+    // remove time from timer
+  } else {
+    console.log("User selected the correct answer");
+    //add to the users score
+  }
+
+  questionIndex++;
+  nextQuestion();
+}
 
 startButton.addEventListener("click", startGame);
