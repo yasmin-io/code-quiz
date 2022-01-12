b72486d39b369d994e0804247747ad2aac243faa
Yasmin <yasmin.bunnie@gmail.com>
2022-01-12 18:57:07 +0000

    readied application for logic input

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,26 @@
+var quizQuestions = [
+  {
+    question: "Question 1",
+    answers: ["A", "B", "C", "D", "E"],
+    correctAnswer: "A",
+  },
+  {
+    question: " Q 2",
+    answers: ["A", "B", "C", "D", "E"],
+    correctAnswer: "B",
+  },
+  {
+    question: "Question 3",
+    answers: ["A", "B", "C", "D", "E"],
+    correctAnswer: "C",
+  },
+  {
+    question: " Q 4",
+    answers: ["A", "B", "C", "D", "E"],
+    correctAnswer: "D",
+  },
+];
+
 var startButton = document.querySelector(".start-button");
 
 function startGame(event) {
